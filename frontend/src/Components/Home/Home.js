adb54ce8ec7700726b1b4d15d4a37033ffc4b691
Sav Ar <%ssasaro@hotmail.com%>
2021-03-18 10:43:04 -0400

    Created DummyData file for testing untilt Django

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,40 +1,9 @@
 import SmallSquare from "../Widgets/SmallSquare/SmallSquare";
 import TaskList from "../Widgets/Task/TaskList/TaskList";
 import {WidgetWrapper} from "./Styles";
-
+import {tasks} from "../../DummyData";
 export default function Home() {
-    // dumy data until Django is setup
-    const tasks = [
-        {
-            "id": "1",
-            "title": "Frontend sprint",
-            "status": "Completed",
-            "description": "Lorem ipsum dolor sit amet.",
-            "PM": "Johnny"
-        },
-        {
-            "id": "2",
-            "title": "Frontend sprint2",
-            "status": "In progress",
-            "description": "Lorem ipsum dolor sit amet.2",
-            "PM": "Johnny2"
-        },
-        {
-            "id": "3",
-            "title": "Frontend sprint3",
-            "status": "Not started",
-            "description": "Lorem ipsum dolor sit amet.3",
-            "PM": "Johnny3"
-        },
-        {
-            "id": "4",
-            "title": "Frontend sprint4",
-            "status": "On hold",
-            "description": "Lorem ipsum dolor sit amet.4",
-            "PM": "Johnny4"
-        },
 
-    ];
     return (
         <div>
             <h1>Home</h1>
---
+++ frontend/src/DummyData.js
@@ -0,0 +1,35 @@
+// dumy data until Django is setup
+
+const tasks = [
+    {
+        "id": "1",
+        "title": "Frontend sprint",
+        "status": "Completed",
+        "description": "Lorem ipsum dolor sit amet.",
+        "PM": "Johnny"
+    },
+    {
+        "id": "2",
+        "title": "Frontend sprint2",
+        "status": "In progress",
+        "description": "Lorem ipsum dolor sit amet.2",
+        "PM": "Johnny2"
+    },
+    {
+        "id": "3",
+        "title": "Frontend sprint3",
+        "status": "Not started",
+        "description": "Lorem ipsum dolor sit amet.3",
+        "PM": "Johnny3"
+    },
+    {
+        "id": "4",
+        "title": "Frontend sprint4",
+        "status": "On hold",
+        "description": "Lorem ipsum dolor sit amet.4",
+        "PM": "Johnny4"
+    },
+
+];
+
+export {tasks};
